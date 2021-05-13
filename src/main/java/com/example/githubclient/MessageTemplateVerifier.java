7ca746867e19dff03265f3a9c6b50e8984bf4bc3
sivalien <sivlaien@icloud.com>
2021-05-13 21:11:21 +0300

    Changed MessageTemplateVerifier.java

--- src/main/java/com/example/githubclient/MessageTemplateVerifier.java
+++ src/main/java/com/example/githubclient/MessageTemplateVerifier.java
@@ -1,9 +1,9 @@
 package com.example.githubclient;
 
 public class MessageTemplateVerifier {
-    private static String VERIFICATION_RESULT = "";
-    private static String PATTERN = "^((LEETCODE|ITERATOR|DESIGN|GENERATOR|TRIANGLE)-(2021|2022)).+";
-    private static String NOT_OK_RESULT = "title should start with GENERATOR, LEETCODE, DESIGN, ITERATOR OR TRIANGLE," +
+    private static final String PATTERN = "^((LEETCODE|ITERATOR|DESIGN|GENERATOR|TRIANGLE)-(2021|2022)).+";
+    private static final String OK_RESULT = "OK!";
+    private static final String NOT_OK_RESULT = "title should start with GENERATOR, LEETCODE, DESIGN, ITERATOR OR TRIANGLE," +
             "group number should be 2021 or 2022";
 
     static boolean check(String message) {
@@ -12,7 +12,7 @@ public class MessageTemplateVerifier {
 
     static String process (String message) {
         if (check(message)) {
-            return VERIFICATION_RESULT;
+            return OK_RESULT;
         }
         return NOT_OK_RESULT;
     }
