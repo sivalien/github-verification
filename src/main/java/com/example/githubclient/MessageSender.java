47d68ca0c5e9dd08f2f5534f3d9f26c8ffe167c6
sivalien <sivlaien@icloud.com>
2021-06-07 19:47:02 +0300

    Changed MessageSender.java

--- src/main/java/com/example/githubclient/MessageSender.java
+++ src/main/java/com/example/githubclient/MessageSender.java
@@ -3,10 +3,19 @@ package com.example.githubclient;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 public class MessageSender {
+    private GithubService service;
+
     @Scheduled(cron = "0/5 * * ? * *")
     void sendMessage() {
         System.out.println("Влад Котов, где ТЗ? Ты же обещал...");
+    }
+
+    @Scheduled(cron = "*/10 * * ? * *")
+    public void check() throws IOException {
+        service.checkPullAndCommit();
     }
 }
